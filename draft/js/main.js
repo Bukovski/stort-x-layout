a8a545b30b9a36d6f9ba8a25ff079aae847824ce
Bukovski <kmanankov@bk.ru>
2019-07-06 15:52:33 +0300

    add: header section without background rectangles

--- draft/js/main.js
+++ draft/js/main.js
@@ -1,4 +1,15 @@
 window.onload = (function () {
   /*********************** my code ***********************/
-  console.log("my code")
+  $('.slider__js').slick({
+    dots: true,
+    arrows: false,
+    
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+  
+    // autoplay: true,
+    autoplaySpeed: 3500
+  });
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ const PATH = {
   ], //поддержка старыми браузерами
   scriptLibs: [
     PROJECT_FOLDERS.START + "_libs/jquery/dist/jquery.min.js",
-    // PROJECT_FOLDERS.START + "_libs/owl.carousel/dist/owl.carousel.min.js"
+    PROJECT_FOLDERS.START + "_libs/slick-carousel/slick/slick.min.js"
   ], //подключаем свои библиотеки
   scriptInput: [
     PROJECT_FOLDERS.START + "js/**/*.js"
